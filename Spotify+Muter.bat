495a1091b2f25b64f52fae10aa2c6f40afe239e8
John Grinalds <62338163+johngrinalds@users.noreply.github.com>
2020-07-16 17:28:51 -0400

    Upload all files

---
+++ Spotify+Muter.bat
@@ -0,0 +1,4 @@
+spotify.exe
+timeout 5 /nobreak
+call C:\ProgramData\Anaconda3\Scripts\activate.bat 
+python "C:\Users\johng\Documents\Github\spotify-admuter\spotipy_ad_muter.py"
---
+++ spotipy_ad_muter.py
@@ -0,0 +1,97 @@
+#pip install spotipy
+#pip install pynput
+
+import sys
+import spotipy
+import spotipy.util as util
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+import time
+
+scope = 'user-read-currently-playing'
+
+
+'''
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+'''
+
+username = "-f"
+
+def get_token():
+    return(util.prompt_for_user_token(username,
+                           scope,
+                           client_id='84d16a6d28d744598e19c4e1bb4923d2',
+                           client_secret='73f0b495c93d4539a43ca771674f1dea',
+                           redirect_uri='http://localhost')
+           )
+
+
+
+#Get Spotify Current Playing Song
+
+def get_playing():
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        result = sp.currently_playing()
+        playing_type = result["currently_playing_type"]
+    else:
+        print("Can't get token for", username)
+    return playing_type
+
+
+def volume_low():
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.volume(volume_percent = 1)
+    else:
+        print("Can't get token for", username)
+
+
+def volume_low():
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.volume(volume_percent = 50)
+    else:
+        print("Can't get token for", username)
+        
+
+
+#These keystrokes open WMC, mute the volume, and close the window
+
+def mute_toggle():
+    keyboard.press(Key.media_volume_mute)
+    keyboard.release(Key.media_volume_mute)
+
+
+#This runs in a loop until cancelled
+
+ 
+print("Be sure Spotify is open and playing.\nRunning... Press Ctrl+C to cancel")
+state = 0 #not playing ad
+token = get_token()
+
+
+
+while True:
+    try:
+        song_status = get_playing()
+        #print("song_status: ", song_status)
+        #print("state: ", state)
+        if (song_status == 'ad')&(state == 0):
+            state = 1
+            mute_toggle()
+        if (song_status != 'ad')&(state == 1):
+            state = 0
+            mute_toggle()
+#    except:
+    except BaseException as error:
+        print('An exception occurred: {}'.format(error))
+        print("Can't connect to Spotify...")
+        token = get_token()
+    time.sleep(0.5)
+
+
